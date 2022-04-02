d249a645685607e3b0c9c825a0f78b8257304e40
319g <lorien.garza@gmail.com>
2022-04-02 18:49:34 +0200

    detalles anadidos al modo oscuro

--- scripts/bgMode.js
+++ scripts/bgMode.js
@@ -3,16 +3,28 @@
 const modToggle = document.getElementById("modToggle");
 const body = document.querySelector("body");
 const applications = document.querySelector(".applications");
+const popupForm = document.querySelector(".popupForm");
+const xmark = document.querySelector(".popupForm i");
 
 const setStyle = ()=>{
     if(window.localStorage.getItem('bgMode') == 'light'){
         modToggle.checked = false;
         body.style.backgroundColor = '#ffffff';
         applications.style.color = '#000000';
+        popupForm.style.backgroundColor = '#ffffff';
+        popupForm.style.color = '#000000';
+        popupForm.style.borderColor = '#000000';
+        xmark.style.backgroundColor = '#ffffff';
+        xmark.style.outlineColor = '#000000';
     }else if(window.localStorage.getItem('bgMode') == 'dark'){
         modToggle.checked = true;
         body.style.backgroundColor = '#1e1e1e';
         applications.style.color = '#ffffff';
+        popupForm.style.backgroundColor = '#1e1e1e';
+        popupForm.style.color = '#ffffff';
+        popupForm.style.borderColor = '#ffffff';
+        xmark.style.backgroundColor = '#1e1e1e';
+        xmark.style.outlineColor = '#ffffff';
     }
 }
 
